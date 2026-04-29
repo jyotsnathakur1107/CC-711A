125ced4f406427d594171b21a27fa1e45f4c0b90
jyotsnathakur1107 <jyotsnathakur1107@gmail.com>
2026-04-29 12:30:32 +0530

    Add files via upload

---
+++ cc_est_practical/counting_bits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countOnes(int n) {
+        int count = 0;
+        while(n!=0){
+            int check = n&1;
+            if(check==1){
+                count++;
+            }
+            n=n>>1;
+        }
+        return count;
+    }
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for (int i = 0; i <= n; i++) {
+            ans.push_back(countOnes(i));
+        }
+        return ans;
+    }
+};
